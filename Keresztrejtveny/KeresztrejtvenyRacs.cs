982137727bc8a035541da7e5b53f676a97226d47
asd <asd@gmail.com>
2026-03-18 08:46:16 +0100

    osztály

---
+++ Keresztrejtveny/KeresztrejtvenyRacs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keresztrejtveny
+{
+	internal class KeresztrejtvenyRacs
+	{
+		List<string> adatsorok;
+		char[,] racs;
+		int[,] sorszamok;
+
+		public int OszlopokDb { get { return adatsorok[0].Length; } }
+		public int SorokDb { get { return adatsorok.Count; } }
+
+		public KeresztrejtvenyRacs(string forras)
+		{
+			BeolvasAdatsorok(forras);
+			racs = new char[SorokDb, OszlopokDb];
+			sorszamok = new int[SorokDb, OszlopokDb];
+			FeltoltRacs(adatsorok);
+		}
+
+		private void BeolvasAdatsorok(string forras)
+		{
+			adatsorok = [];
+			using (var olvaso = new StreamReader(forras))
+			{
+				while (!olvaso.EndOfStream)
+				{
+					adatsorok.Add(olvaso.ReadLine());
+				}
+			}
+		}
+
+		private void FeltoltRacs(List<string> adatsorok)
+		{
+			for (int i = 0; i < SorokDb; i++)
+			{
+				for (int j = 0; j < OszlopokDb; j++)
+				{
+					racs[i, j] = adatsorok[i][j];
+				}
+			}
+		}
+	}
+}
